fb656e0b4df2318d02874300e4705f45190d85bb
Jaaliyah <jaaliyah_alexander@outlook.com>
2023-12-15 14:01:28 -0900

    static files

--- personal_bujo/settings.py
+++ personal_bujo/settings.py
@@ -114,7 +114,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
@@ -129,7 +129,7 @@ LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "home" 
 LOGOUT_REDIRECT_URL = "login"
 
-import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
